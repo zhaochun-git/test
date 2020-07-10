5da63bcd0d0d25111b466222fb1f0967ee7ec14d
zhaochun-git <13269373670@163.com>
2020-07-10 21:50:27 +0800

    shanshanxiugai

--- ideagit/src/main/java/com/atguigu/idea/Zhaochun.java
+++ ideagit/src/main/java/com/atguigu/idea/Zhaochun.java
@@ -5,6 +5,7 @@ package com.atguigu.idea;
 public class Zhaochun {
     public static void main(String[] args) {
         System.out.println("aishanshan");
+        System.out.println("henaishanshan");
         //woaini
     }
 }
